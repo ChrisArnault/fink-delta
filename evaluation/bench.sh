6f277252ad6c66b34422d1dd17567ce8d474ffd1
Christian Arnault <arnault@lal.in2p3.fr>
2019-12-10 17:30:48 +0100

    evaluate> augment the bench

--- evaluation/bench.sh
+++ evaluation/bench.sh
@@ -1,11 +1,13 @@
 
-delta.sh format=parquet batch_size=1 >delta_parquet_bs1.txt 
-delta.sh format=parquet batch_size=10 >delta_parquet_bs10.txt 
-delta.sh format=parquet batch_size=100 >delta_parquet_bs100.txt 
+# delta.sh format=parquet batch_size=1 >delta_parquet_bs1.txt
+# delta.sh format=parquet batch_size=10 >delta_parquet_bs10.txt
+# delta.sh format=parquet batch_size=100 >delta_parquet_bs100.txt
+delta.sh format=parquet batch_size=1000 >delta_parquet_bs1000.txt
 
-delta.sh format=delta batch_size=1 >delta_delta_bs1.txt
-delta.sh format=delta batch_size=10 >delta_delta_bs10.txt
-delta.sh format=delta batch_size=100 >delta_delta_bs100.txt
+# delta.sh format=delta batch_size=1 >delta_delta_bs1.txt
+# delta.sh format=delta batch_size=10 >delta_delta_bs10.txt
+# delta.sh format=delta batch_size=100 >delta_delta_bs100.txt
+delta.sh format=delta batch_size=1000 >delta_parquet_bs1000.txt
 
 
 
