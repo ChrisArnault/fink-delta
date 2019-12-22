1b6f473be6349f7a334f8103b4ddbda25c00eec0
Arnault <arnault@lal.in2p3.fr>
2019-12-22 13:34:41 +0100

    evaluation> check.py parsing times

--- evaluation/check.py
+++ evaluation/check.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,6 +16,9 @@ class Value(object):
         self.time = 0
         self.size = 0
 
+    def __str__(self):
+        return "time={} zize={}".format(self.time, self.size)
+
 
 def check(num, file_name):
     with open(file_name) as f:
@@ -27,7 +31,7 @@ def check(num, file_name):
     start_batch = False
     value = None
 
-    keys = ("batch #", "| 0h0m", "increment=")
+    keys = ("batch #", "| ", "increment=")
 
     for line in content:
         if "batch #" in line:
@@ -36,19 +40,19 @@ def check(num, file_name):
             start_batch = True
         if start_batch and (keys[1] in line):
             t = line.split(keys[1])[1]
-            t = t.split("s")
-            value.time = float(t[0])
-            print("time={}".format(value.time))
+            m = re.match("([0-9]+)[h]([0-9]+)m([0-9]+)[.]([0-9]+)[s]", t)
+            value.time = float(m.group(1))*3600.0 + float(m.group(2))*60.0 + float(m.group(3)) + float(m.group(4))
+            # print("time={}".format(value.time))
         if start_batch and (keys[2] in line):
             t = line.split(keys[2])[1]
+            t = t.split(" ")[0]
             size = float(t)
-            print("size={}".format(size))
             value.size = size
             values.append(value)
             start_batch = False
 
     # values = [22.581, 20.183, 19.148, 18.585, 17.347, 19.156, 15.076, 22.181, 20.943, 19.537]
-    print(values)
+    # print("values=", values)
 
 
     if len(values) > 0:
